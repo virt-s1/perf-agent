57983b47ae1fb61b42577e1a80561f5b4049dc67
Charles Shih <schrht@gmail.com>
2020-11-11 13:46:01 +0800

    Enhance the script.

--- pbench_setup/bin/azure_rhui_eus_workaround.sh
+++ pbench_setup/bin/azure_rhui_eus_workaround.sh
@@ -5,9 +5,9 @@
 #   This script does the following actions:
 #   1. Check whether the instance is confiured with RHUI EUS repos.
 #   2. Try to frozen the $releasever for the RHUI EUS repos.
-#   3. Disable the /etc/yum/vars/releasever configuration.
+#   3. Update the $releasever configuration.
 
-# check whether the instance is confiured with RHUI EUS repos.
+# check whether the instance is confiured with RHUI EUS repos
 found=$(ls /etc/yum.repos.d/rh-cloud*eus*.repo 2>/dev/null | wc -l)
 [ "$found" == "0" ] && echo "No RHUI EUS repos is configured." && exit 0
 
@@ -15,18 +15,27 @@ found=$(ls /etc/yum.repos.d/rh-cloud*eus*.repo 2>/dev/null | wc -l)
 [ "$found" != "1" ] && echo "Multiple repo files were found." && exit 1
 rpfile=$(ls /etc/yum.repos.d/rh-cloud*eus*.repo) || exit 1
 
+# check whether the $releasever is already frozen
+grep -q '$releasever' $rpfile || exit 0
+
 # backup the file if haven't yet
 [ ! -e ${rpfile}.orginal ] && cp $rpfile ${rpfile}.orginal
 
 # get current $releasever
-releasever=$(cat /etc/yum/vars/releasever) || exit 1
+releasever=$(cat /etc/yum/vars/releasever 2>/dev/null)
 [ -z "$releasever" ] && echo "Failed to get \$releasever." && exit 1
 
-# Frozen the $releasever for the RHUI EUS repos.
+# frozen the $releasever for the RHUI EUS repos
 echo "Forzen release version for RHUI repos to \"$releasever\"."
 sed -i "s/\$releasever/$releasever/g" $rpfile
 
-# Disable the /etc/yum/vars/releasever configuration
-mv /etc/yum/vars/releasever /etc/yum/vars/releasever.bak
+# set the new $releasever
+releasever=${releasever%%.*}
+
+# update the $releasever configuration
+rlsfile=/etc/yum/vars/releasever
+echo "Set the release version in $rlsfile to \"$releasever\"."
+[ ! -e ${rlsfile}.orginal ] && cp $rlsfile ${rlsfile}.orginal
+echo $releasever >$rlsfile
 
 exit 0
