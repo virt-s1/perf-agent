c8452ae71c7faaff744722f10726829487f4c196
Charles Shih <schrht@gmail.com>
2020-12-08 22:27:37 +0800

    Add script to write metadata entries to json file.

---
+++ pbench_runner/write_metadata.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""Write metadata to a json file.
+
+Write user provided metadata entries to a specified json file.
+"""
+
+import argparse
+import os
+import json
+
+ARG_PARSER = argparse.ArgumentParser(description="Write user provided \
+metadata entries to a specified json file.")
+
+ARG_PARSER.add_argument('--file',
+                        dest='file',
+                        action='store',
+                        help='The file stores the metadata, will create a \
+new one if not found.',
+                        default='./testrun_metadata.json',
+                        required=False)
+ARG_PARSER.add_argument('--keypair',
+                        dest='keypair',
+                        action='append',
+                        help='The metadata in KEY=VALUE format to be added \
+or updated. This argument can be provided multiple times.',
+                        required=True)
+
+ARGS = ARG_PARSER.parse_args()
+
+if __name__ == '__main__':
+
+    # Read the metadata entries or init from scratch
+    if os.path.isfile(ARGS.file):
+        with open(ARGS.file, 'r') as f:
+            metadata = json.load(f)
+    else:
+        metadata = {}
+
+    # Update the metadata entries
+    for keypair in ARGS.keypair:
+        key, value = keypair.split('=')
+        metadata[key] = value
+
+    # Write the metadata entries
+    with open(ARGS.file, 'w') as f:
+        json.dump(metadata, f, indent=3, sort_keys=True)
+
+    exit(0)
